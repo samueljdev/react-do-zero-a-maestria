487eabb96b399400948e7f0b4c52fdeb539332df
samue <samuel.jdev11@gmail.com>
2024-03-29 10:28:14 -0400

    02-imagem em src

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 
+import City from "./assets/city.jpg"
+
 function App() {
 
   return (
@@ -7,6 +9,9 @@ function App() {
       <h1>Pilares do React</h1>
       <div>
         <img src='/img1.jpg' alt='Paisagem' />
+      </div>
+      <div>
+        <img src={City} alt='Paisagem' />
       </div>
     </>
   )
