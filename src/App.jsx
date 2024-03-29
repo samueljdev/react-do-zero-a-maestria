47c9d75b247b8585efac58dcaa9d1425657e9ddc
samue <samuel.jdev11@gmail.com>
2024-03-29 09:51:45 -0400

    02-template expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
 
 function App() {
 
   return (
     <>
       <FirstComponent />
+      <TemplateExpressions />
     </>
   )
 }
---
+++ src/components/TemplateExpressions.jsx
@@ -0,0 +1,20 @@
+const TemplateExpressions = () => {
+    const nome = "Samuel"
+    const data = {
+        idade: 34,
+        ocupacao: "programador"
+    }
+
+    return (
+        <div>
+            <h1>Componente TemplateExpressions</h1>
+            <p>Meu nome é : {nome}</p>
+            <p>Minha idade é : {data.idade}</p>
+            <p>Minha ocupacao é : {data.ocupacao}</p>
+            <p>Soma 4 + 4 = {4 + 4}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default TemplateExpressions;
