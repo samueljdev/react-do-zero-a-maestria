406dc87fa8012d1e2504ea2f7fd7322a7c29c1c2
samue <samuel.jdev11@gmail.com>
2024-03-30 11:32:35 -0400

    02-label envolvendo input

--- src/components/Myform.jsx
+++ src/components/Myform.jsx
@@ -9,6 +9,11 @@ const Myform = () => {
                     <label htmlFor="nome">Nome:</label>
                     <input type="text" nome="nome" placeholder="Digite o seu nome" />
                 </div>
+                {/* Label envolvendo input(ABORDAGEM SUGERIDA NA DOCUMENTACAO) */}
+                <label>
+                    <span>Email</span>
+                    <input type="email" nome="email" placeholder="Digite o seu email" />
+                </label>
                 <input type="submit" value="Enviar" />
             </form>
         </>
