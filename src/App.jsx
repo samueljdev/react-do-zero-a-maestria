f68b98228bdcce9cdd44564a5e1ec7c2d24b575c
samue <samuel.jdev11@gmail.com>
2024-03-29 21:15:40 -0400

    02-css de componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
     <>
-        <h1>React com CSS</h1>
+      <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este é o paragrafo é do App.jsx</p> {/* o CSS de componente ele vaza*/}
+      <p className="my-paragrafo">Este tbm é do componente</p>
     </>
   )
 }
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css" // CSS de componente ele vaza
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>CSS de componente </h1>
+            <p>Este é o paragrafo do componente</p>
+        </div>
+    )
+}
+
+export default MyComponent
