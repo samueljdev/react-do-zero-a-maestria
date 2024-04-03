94b106403bf00eedf543ce40453bb871b04388e8
samue <samuel.jdev11@gmail.com>
2024-04-03 12:10:39 -0400

    03-adicionando produtos com react

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const url = "http://localhost:3000/products"
 
 function App() {
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
   // resgatando os dados
   useEffect(() => {
@@ -17,16 +19,58 @@ function App() {
     fetchData()
   }, [])
 
+  // função que vai enviar um formulario
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name: name,
+      price: price,
+    }
+
+    const requisicao = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "aplication/json"
+      },
+      body: JSON.stringify(product)
+    })
+  }
+
   return (
-    <>
+    <div className='App'>
       <h1>Lista de Produtos</h1>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            {product.name} - {product.price}</li>
+            {product.name} - {product.price}
+          </li>
         ))}
       </ul>
-    </>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name='name'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name='price'
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value={"Criar"} />
+        </form>
+      </div>
+    </div>
   )
 }
 
