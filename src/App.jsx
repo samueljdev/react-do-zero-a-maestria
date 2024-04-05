d8b0652675dfeda600c4df26314a3c9c15a309cb
samue <samuel.jdev11@gmail.com>
2024-04-05 12:30:52 -0400

    01-configurando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,6 +6,10 @@ function App() {
   return (
     <>
       <h1>07-React-router</h1>
+      <BrowserRouter />
+      <Routes> {/* elementos fora do Routes irao se repetir em todas as páginas*/}
+        <Route path="/" element="" />
+      </Routes>
     </>
   )
 }
